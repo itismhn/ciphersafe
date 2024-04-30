1c6af4db7a2b57b0653a191fd76637f1c1b803e8
Mohammad Hossein Nouri <mmdnoori13800@gmail.com>
2024-04-30 22:06:31 +0330

    make banner better

--- ciphersafe.py
+++ ciphersafe.py
@@ -6,9 +6,9 @@ def main():
   / __(_)_ __| |_  ___ _ _/ __| __ _ / _|___ 
  | (__| | '_ \ ' \/ -_) '_\__ \/ _` |  _/ -_)
   \___|_| .__/_||_\___|_| |___/\__,_|_| \___|
-        |_|
-@itisMHN v1.1 - https://github.com/itismhn/ciphersafe
+@itisMHN|_|V.1.1 https://github.com/itismhn/ciphersafe
 """)
+# base URL of the API
 base_url = "https://ciphersuite.info/api"
 
 if __name__ == "__main__":
