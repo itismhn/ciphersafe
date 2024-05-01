42f1c1896a4e28c7b41814b4932177e25fc5740e
itismhn <itismhn@yahoo.com>
2024-05-01 12:27:01 +0330

    collorize secure field and change other formats

--- ciphersafe.py
+++ ciphersafe.py
@@ -1,12 +1,19 @@
 import requests
 import argparse
+
+COLOR_BOLD = "\033[1m"
+COLOR_RESET = "\033[0m"
+COLOR_RED = "\033[91m"
+COLOR_GREEN = "\033[32m"
+COLOR_YELLOW = "\033[93m"
+
 def main():
     print("""
    ___ _      _            ___        __     
   / __(_)_ __| |_  ___ _ _/ __| __ _ / _|___ 
- | (__| | '_ \ ' \/ -_) '_\__ \/ _` |  _/ -_)
-  \___|_| .__/_||_\___|_| |___/\__,_|_| \___|
-@itisMHN|_|V.1.1 https://github.com/itismhn/ciphersafe
+ | (__| | '_ \\ ' \\/ -_) '_\\__ \\/ _` |  _/ -_)
+  \\___|_| .__/_||_\\___|_| |___/\\__,_|_| \\___|
+@itisMHN|_|V.1.1 github.com/itismhn/ciphersafe
     """)
     # Create the argument parser
     parser = argparse.ArgumentParser(description="check information of tls cipher suites from the Ciphersuite.info API")
@@ -28,8 +35,25 @@ def get_cipher_suite(cipher_suite_name):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        print("Security: {}".format(data[cipher_suite_name]['security']))
-
+        cipher_info = data[cipher_suite_name]
+        print(COLOR_BOLD + "Cipher Suite:", cipher_suite_name + COLOR_RESET)
+        print("Security:", end=" ")
+        security_status = cipher_info.get('security', 'N/A')
+        if security_status == 'secure':
+            print(COLOR_GREEN + security_status + COLOR_RESET)
+        elif security_status == 'weak':
+            print(COLOR_YELLOW + security_status + COLOR_RESET)
+        else:
+            print(COLOR_RED + security_status + COLOR_RESET)
+        print("TLS Version:", cipher_info.get('tls_version', 'N/A'))
+        print("Hex Byte 1:", cipher_info.get('hex_byte_1', 'N/A'))
+        print("Hex Byte 2:", cipher_info.get('hex_byte_2', 'N/A'))
+        print("Protocol Version:", cipher_info.get('protocol_version', 'N/A'))
+        print("Key Exchange Algorithm:", cipher_info.get('kex_algorithm', 'N/A'))
+        print("Authentication Algorithm:", cipher_info.get('auth_algorithm', 'N/A'))
+        print("Encryption Algorithm:", cipher_info.get('enc_algorithm', 'N/A'))
+        print("Hash Algorithm:", cipher_info.get('hash_algorithm', 'N/A'))
+        
 
 
 
