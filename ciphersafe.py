3adab1b42caab1c4103a2a5f83f77594f4459e93
Mohammad Hossein Nouri <mmdnoori13800@gmail.com>
2025-05-12 13:16:47 +0330

    V.1.1 -> V.2.0

--- ciphersafe.py
+++ ciphersafe.py
@@ -20,7 +20,7 @@ def main():
   / __(_)_ __| |_  ___ _ _/ __| __ _ / _|___ 
  | (__| | '_ \\ ' \\/ -_) '_\\__ \\/ _` |  _/ -_)
   \\___|_| .__/_||_\\___|_| |___/\\__,_|_| \\___|
-{}@itisMHN{}|_|{}V.1.1 github.com/itismhn/ciphersafe{}
+{}@itisMHN{}|_|{}V.2.0 github.com/itismhn/ciphersafe{}
     """.format(COLOR_WHITE, COLOR_GREEN, COLOR_RESET, COLOR_GREEN, COLOR_RESET)
     print(banner)
 
